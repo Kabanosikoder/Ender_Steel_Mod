7aaee07db9131f2ab8557742068b903094b015bf
Panther Kabanosikoder <kflodab@gmail.com>
2024-10-21 16:00:22 +0200

    Initial commit :3

---
+++ src/main/java/net/panther/endersteel/EnderSteelClient.java
@@ -0,0 +1,10 @@
+package net.panther.endersteel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderSteelClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/panther/endersteel/EnderSteelDataGenerator.java
@@ -0,0 +1,11 @@
+package net.panther.endersteel;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnderSteelDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

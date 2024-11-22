ad3a3a0273c3b4ed4c5e1fbe291dc8dee62936f2
Panther Kabanosikoder <kflodab@gmail.com>
2024-11-22 05:40:21 +0100

    ACTUALLY fixed mod id

--- src/main/java/net/panther/endersteel/EnderSteel.java
+++ src/main/java/net/panther/endersteel/EnderSteel.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EnderSteel implements ModInitializer {
-	public static final String MOD_ID = "ender-steel";
+	public static final String MOD_ID = "endersteel";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
